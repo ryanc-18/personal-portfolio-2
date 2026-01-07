acab9083952879e421076665de01151b06de9ba2
Ryan Chin <ryankhayvyei@student.unimelb.edu.au>
2026-01-07 17:04:06 +0800

    built the base homepage divided into sections and added minimal text

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,41 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="bg-black min-h-screen p-2 fixed inset-0 overflow-auto">
+      <main className="min-h-full bg-gray-100 rounded-2xl">
+        <div className="fixed py-6 px-10 z-50 flex top-0 left-0 right-0 items-center">
+          <div className="text-lg text-black">
+            RYAN CHIN
+          </div>
+          <nav className="flex gap-6 left-1/2 -translate-x-1/2 absolute">
+            <a href="#about" className="text-black hover:underline">About</a>
+            <a href="#projects" className="text-black hover:underline">Projects</a>
+            <a href="#contact" className="text-black hover:underline">Contact</a>
+          </nav>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="pt-16 px-8">
+          <p className="text-black text-4xl">My Profile</p>
+          <div className="grid grid-cols-5 gap-4 h-[calc(100vh-180px)] mt-4">
+            <div className="col-span-3 bg-white rounded-2xl p-4" id="about">
+              <p className="text-black text-3xl mb-7">About Me</p>
+              <p className="text-black text-1xl mb-7">I am a passionate Software Developer with a knack for building full-stack web applications using modern technologies like <strong>Next.js</strong> and <strong>Tailwind CSS</strong>. My Journey in tech began with a curiosity for solving real-world problems through innovative solutions, which evolved into a love for crafting user-centric digital experiences.</p>
+              <p className="text-black text-1xl">With growing experience in JavaScript frameworks, I'm focused on building scalable and efficient applications. With a solid foundation in backend development, including experience with <strong>Node.js</strong>, <strong>Express</strong>, <strong>Adonis.js</strong>, I'm now diving deeper into frontend development and full-stack applications with <strong>Next.js</strong>, combining my server-side expertise with modern frontend technologies.</p>
+            </div>
+
+            <div className="flex flex-col gap-4 col-span-2" id="projects">
+              <div className="bg-white rounded-2xl p-4" id="project1">
+                <p>project 1 description</p>
+              </div>
+
+              <div className="bg-white rounded-2xl p-4" id="project2">
+                <p>project 2 description</p>
+              </div>
+
+              <div className="bg-white rounded-2xl p-4" id="project3">
+                <p>project 3 description</p>
+              </div>
+            </div>
+
+          </div>
         </div>
       </main>
     </div>
